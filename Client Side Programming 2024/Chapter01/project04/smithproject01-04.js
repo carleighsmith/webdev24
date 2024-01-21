6c72ceab2792b1a742d46a5028c055198ddcfb00
carleighsmith32 <carleigh.smith.32@student.prcc.edu>
2024-01-21 16:02:24 -0600

    Client Side Projects

---
+++ Client Side Programming 2024/Chapter01/project04/smithproject01-04.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Carleigh Smith
+    Date:   01/21/2024
+
+    Filename: smithproject01-04.js
+*/
+
+//define variables for home and work addresses
+
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+
+let workStreet = "15 Oak Ln.";
+let workCity = "Central City";
+let workState = "MA";
+let workCode = "02104";
