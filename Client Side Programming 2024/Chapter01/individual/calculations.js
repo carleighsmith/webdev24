ce2cef95b9e01ca2a48ec0ccc08c3778f252c90e
carleighsmith32 <carleigh.smith.32@student.prcc.edu>
2024-03-15 22:12:38 -0500

    Client Side

--- Client Side Programming 2024/Chapter01/individual/calculations.js
+++ Client Side Programming 2024/Chapter01/individual/calculations.js
@@ -22,27 +22,31 @@ function verifyForm() {
     try {
         if ((age) && (min) && (max))
         {
-            if (age < 5)
-                estimate += 50;
+            if (age<=0 || min <=0 || max <= 0 || max <= min)
+            {
+                window.alert("Make sure every field entry is a number over 0, make sure the Maximum balance is more than the minimum, and make sure only numbers are entered.")
+            }else
+            {
+                if (age < 5){
+                    estimate += 50;
             
-            average = max-min;
-            estimate += average
-            estimate = Math.round(estimate) 
-            window.alert("Your estimated total is $"+ estimate)
-            addPetPhotos();
-        } else {
-            window.alert("Please fill in all fields");
+                    average = max-min;
+                    estimate += average
+                    estimate = Math.round(estimate) 
+                    window.alert("Your estimated total is $"+ estimate)
+                    addPetPhotos();
+                } else 
+                {window.alert("Please fill in all fields");}
+            }      
         }
-
+        else {
+            window.alert("Please fill in all fields");
+        
+        }
     } catch (err)
     {
         window.alert(err)
     }
-
-    //(age) && (min) && (max)
-    // ? window.alert("Your estimated total would be $" + estimate) :
-    // window.alert("Please fill in all fields");
-
 
 }
 
@@ -70,6 +74,7 @@ function addPetPhotos()
     name[11]= "Anthony";
     name[12]= "May";
     name[13]= "Sky";
+    name[14]= "Monet";
 
     let age = new Array(14);// will add more
     age[0] = "2 years";
@@ -86,12 +91,13 @@ function addPetPhotos()
     age[11] = "1 year";
     age[12] = "4 years";
     age[13] = "6 years";
+    age[14] = "4 months";
 
 
     let htmlCode = "";
 
 
-    for (let i = 1; i < 15; i++)
+    for (let i = 1; i < 2; i++)
     {
         htmlCode += '<figure>' + 
         '<img alt ="" src="images/slide' + i + '.jpg" />' +
@@ -100,6 +106,188 @@ function addPetPhotos()
                 '</figure>';
     }
 
-    document.getElementById("gallery").innerHTML = htmlCode;
+    document.getElementById("gallerya").innerHTML = htmlCode;
 
+    htmlCode = "";
+
+    for (let i = 2; i < 3; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryb").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 3; i < 4; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryc").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 4; i < 5; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg"/>' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryd").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 5; i < 6; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("gallerye").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 6; i < 7; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryf").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 7; i < 8; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryg").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 8; i < 9; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryh").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 9; i < 10; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryi").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 10; i < 11; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryj").innerHTML = htmlCode;
+
+
+    htmlCode = "";
+
+    for (let i = 11; i < 12; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryk").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 12; i < 13; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryl").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 13; i < 14; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("gallerym").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 14; i < 15; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryn").innerHTML = htmlCode;
+
+    htmlCode = "";
+
+    for (let i = 15; i < 16; i++)
+    {
+        htmlCode += '<figure>' + 
+        '<img alt ="" src="images/slide' + i + '.jpg" />' +
+                '<figcaption> Name: ' +name[i-1] +'</figcaption>' +
+                '<figcaption> Age: ' +age[i-1] +'</figcaption>'+
+                '</figure>';
+    }
+
+    document.getElementById("galleryo").innerHTML = htmlCode;
 }
