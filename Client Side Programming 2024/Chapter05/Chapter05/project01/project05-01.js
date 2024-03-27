d2d57ad13003931cf1c2a574e4c7ece67dbd744d
carleighsmith32 <carleigh.smith.32@student.prcc.edu>
2024-03-27 12:28:36 -0500

    HTML Your Turn Chapter 8

--- Client Side Programming 2024/Chapter05/Chapter05/project01/project05-01.js
+++ Client Side Programming 2024/Chapter05/Chapter05/project01/project05-01.js
@@ -24,3 +24,51 @@ let overlay = document.getElementById("overlay");
 // Initialize the quiz time
 quizClock.value = quizTime;
 let timeLeft = quizTime;
+
+//Declare the ID for timed commands
+// and the node list for questions
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
+
+startQuiz.onclick = function() {
+      overlay.className = "showquiz";
+      timeID = setInterval(countdown, 1000);
+}
+
+//Countdown function
+function countdown() {
+      if (timeLeft === 0)
+      {
+            clearInterval(timeID);
+            let totalCorrect = checkAnswers();
+            if (totalCorrect === correctAnswers.length) {
+                  window.alert("Congratulations! You scored 100%");
+            }
+            else {
+                  window.alert("You got " + totalCorrect + "/" + correctAnswers.length);
+                  timeLeft = quizTime;
+                  quizClock.value = timeLeft;
+                  overlay.className = "hidequiz";
+
+            }
+      } else{
+            timeLeft--;
+            quizClock.value = timeLeft;
+      }
+}
+
+function checkAnswers() {
+      let correctCount = 0;
+
+      for (let i = 0; i < questionList.length; i++) {
+            if (questionList[i].value === correctAnswers[i])
+            {
+                  correctCount++;
+                  questionList[i].className = "";
+            } else {
+                  questionList[i].className = "wronganswer";
+            }
+      } 
+      return correctCount;
+}
+    
--- Client Side Programming 2024/Chapter05/Chapter05/project02/project05-02.js
+++ Client Side Programming 2024/Chapter05/Chapter05/project02/project05-02.js
@@ -4,9 +4,31 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: 
-      Date:   
+      Author: Carleigh Smith
+      Date:   3/26/2024
 
       Filename: project05-02.js
 */
+// Declare the following variables
+var images = document.getElementsByTagName("img"); 
+var photoBucket = document.getElementById("photo_bucket"); 
+var photoList = document.getElementById("photo_list");
 
+
+for (let i = 0; i < images.length; i++) {
+  
+    images[i].onclick = function() {
+
+        if (this.parentElement.id === "photo_bucket") {
+       
+            let newItem = document.createElement("li");
+         
+            photoList.appendChild(newItem);
+            newItem.appendChild(this);
+        } else {
+            let oldItem = this.parentElement;
+            photoBucket.appendChild(this);
+            oldItem.parentElement.removeChild(oldItem);
+        }
+    };
+}
