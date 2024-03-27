e1ed59368b3dea7398c1c279b5198e48819fa1b1
carleighsmith32 <carleigh.smith.32@student.prcc.edu>
2024-03-27 13:47:07 -0500

    Webdev

--- Client Side Programming 2024/Chapter06/Chapter06/project01/project06-01.js
+++ Client Side Programming 2024/Chapter06/Chapter06/project01/project06-01.js
@@ -10,3 +10,33 @@
       Filename: project06-01.js
 */
 
+//declare the following variables: submitButton 
+//referencing the element with the id “submitButton”, 
+//pwd referencing the element with the id “pwd”, and 
+//pwd2 referencing the element with the id “pwd2”.
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+
+// event listener for the click event occurring with the 
+//submitButton that runs an anonymous function
+// add event listener for submit button click
+submitButton.addEventListener("click",function(){
+
+      var pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    // if the password is invalid, display an error message
+    if (!pwdRegex.test(pwd.value)) 
+    {
+        pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number.");
+    } 
+    else if (!(pwd.value === pwd2.value)) 
+    {
+        pwd2.setCustomValidity("Your passwords must match.");
+    } else {
+        pwd.setCustomValidity("");
+        pwd2.setCustomValidity("");
+    }
+});
+
