228c243966127edabcb69828329b7dca4f5235f2
carleighsmith32 <carleigh.smith.32@student.prcc.edu>
2024-05-01 00:57:46 -0500

    Final Project

--- poochParlor-bootsrap/scripts/scripts.js
+++ poochParlor-bootsrap/scripts/scripts.js
@@ -3,3 +3,8 @@
     File Name: script.js
     Date: 4/17/2024
 */
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
